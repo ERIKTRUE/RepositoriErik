1ba5eb4e429eaeda7a934affbacf5455c4dd5b6a
TNB-20 <TNB-20@DELL-119>
2019-03-19 09:50:00 +0100

    Summarize changes in around 50 characters or less
    More detailed explanatory text, if necessary. Wrap it to about 72
    characters or so. In some contexts, the first line is treated as the
    subject of the commit and the rest of the text as the body. The
    blank line separating the summary from the body is critical (unless
    you omit the body entirely); various tools like `log`, `shortlog`
    and `rebase` can get confused if you run the two together.
    
    Explain the problem that this commit is solving. Focus on why you
    are making this change as opposed to how (the code explains that).
    Are there side effects or other unintuitive consequences of this
    change? Here's the place to explain them.
    
    Further paragraphs come after blank lines.
    
     - Bullet points are okay, too
    
     - Typically a hyphen or asterisk is used for the bullet, preceded
       by a single space, with blank lines in between, but conventions
       vary here
    
    If you use an issue tracker, put references to them at the bottom,
    like this:
    
    Resolves: #123
    See also: #456, #789

--- arrayLists/src/arrayLists/exempleArrayList.java
+++ arrayLists/src/arrayLists/exempleArrayList.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class exempleArrayList {
 	public static void main(String[] args) {
+		private int numero;
 		
 		ArrayList<Integer> numeros = new ArrayList();
 
-		int i = (int) (Math.random() * 20);
+		int i = (int) (Math.random() * 40);
 		while (i != 0) {
 			numeros.add(i);
 		i = (int) (Math.random() * 11 - Math.random() * 11);
